3f9495ba769993fcdbf6a43f31d89c219f53dc9a
hadikhah <ma.hadikhah@raahin.com>
2022-02-14 11:57:59 +0330

    rtl support for sidebar

--- src/components/Sidebar/SidebarContent.js
+++ src/components/Sidebar/SidebarContent.js
@@ -13,7 +13,7 @@ function Icon({ icon, ...props }) {
 function SidebarContent() {
   return (
     <div className="py-4 text-gray-500 dark:text-gray-400">
-      <a className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
+      <a className="ms-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
         Windmill
       </a>
       <ul className="mt-6">
@@ -21,20 +21,21 @@ function SidebarContent() {
           route.routes ? (
             <SidebarSubmenu route={route} key={route.name} />
           ) : (
-            <li className="relative px-6 py-3" key={route.name}>
+            <li className="relative  py-3" key={route.name}>
+              <Route path={route.path} exact={route.exact}>
+                <span
+                  className="absolute inset-y-0 start-0 more-start-8 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
+                  aria-hidden="true"
+                ></span>
+              </Route>
               <NavLink
                 exact
                 to={route.path}
-                className="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
+                className="px-4 inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
                 activeClassName="text-gray-800 dark:text-gray-100"
               >
-                <Route path={route.path} exact={route.exact}>
-                  <span
-                    className="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
-                    aria-hidden="true"
-                  ></span>
-                </Route>
-                <Icon className="w-5 h-5" aria-hidden="true" icon={route.icon} />
+
+                <Icon className="w-5 m-2 h-5" aria-hidden="true" icon={route.icon} />
                 <span className="ml-4">{route.name}</span>
               </NavLink>
             </li>
